dfc21f6501288dbd970362a0698052543eaa5f03
Cpt Jack Sparrow <118166563+CptEugeneSparrow@users.noreply.github.com>
2023-01-31 21:18:18 +0300

    Initial commit

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,28 @@
+//
+//  MyCustomView.swift
+//  Netology_IB_Instruments
+//
+//  Created by new owner on 29.01.2023.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var nameLable: UILabel!
+
+    @IBOutlet weak var birthdayLabel: UILabel!
+
+    @IBOutlet weak var cityLabel: UILabel!
+
+    @IBOutlet weak var textView: UITextView!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by new owner on 29.01.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    // MARK: - Properties
+
+    private var myView: ProfileView!
+
+    // MARK: - Lifecycle
+
+    override func loadView() {
+        super.loadView()
+        setup()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+extension ProfileViewController {
+
+    private func setup() {
+        myView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView
+        self.view = myView
+    }
+}
